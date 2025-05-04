689933cc6ab1de994d0bcf429ea5cbb1b5533fdf
Rafay Salahuddin <rsalahuddin@varicent.com>
2025-05-03 22:22:33 -0400

    remove gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ghPages from "vite-plugin-gh-pages";
-
-const repoName = "JobFitAI";
 
 export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [react(), ghPages()],
+  plugins: [react()],
 });
